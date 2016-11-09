be062ce03723665dc68bdf5469f459de28dbd8a2
Mario Canto <mcanto@The-Great-Pumpkin.local>
2016-11-09 10:03:45 -0600

    Added Enterprise Business Rules Module

---
+++ EnterpriseBusinessRules/EnterpriseBusinessRules/EnterpriseBusinessRules.h
@@ -0,0 +1,19 @@
+//
+//  EnterpriseBusinessRules.h
+//  EnterpriseBusinessRules
+//
+//  Created by Mario Canto on 11/9/16.
+//  Copyright © 2016 Nearshore Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EnterpriseBusinessRules.
+FOUNDATION_EXPORT double EnterpriseBusinessRulesVersionNumber;
+
+//! Project version string for EnterpriseBusinessRules.
+FOUNDATION_EXPORT const unsigned char EnterpriseBusinessRulesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnterpriseBusinessRules/PublicHeader.h>
+
+
